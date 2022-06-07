eb2f3f7d3e900e449defa982659b47dc78c97bc6
unknown <harsh.kumar.cse20@itbhu.ac.in>
2022-06-07 18:53:57 +0530

    added file to gitignore

--- call_test.py
+++
@@ -1,4 +0,0 @@
-from test import func
-func()
-print("This functio in Not noe")
-print("This function is running")
--- test.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-from preprocessing import image_loading
-
-plt.imshow(image_loading('content/myntradataset/images/2138.jpg',False,[300,300]))
-plt.show()
